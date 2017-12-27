ab12c8cfa320c0c485105a50b3a5cd84fa44057b
Jebbss <jebsumerac@gmail.com>
2017-12-27 13:15:18 -0600

    Debugging

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require('express')
 var app = express()
 
+
+var port = process.env.PORT || 3500;
+
+app.listen(port, function(){
+  console.log("Listening on port: " + port);
+});
+
 app.get('/', function (req, res) {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   lang =  req.headers["accept-language"].split(',')[0];
   system =  req.headers['user-agent'].split(/[\(\)]/)[1];
   res.send({"ipaddress":ip,"language":lang,"system":system})
-})
-
-app.listen(8000, function () {
-  console.log('Listening on port 8000!')
 })
